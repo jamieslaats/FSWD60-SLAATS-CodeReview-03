22d82f72771eaf8b5c4c8896b3fbe0d4b35ff8cf
Jamie Slaats <jjslaats@mailbox.org>
2019-01-28 20:47:36 +0100

    updated javascript code, working more than before.

--- js/carinsurance.js
+++ js/carinsurance.js
@@ -6,37 +6,56 @@
 	var activeDate = ("Todays Date: "+ day + "." + month + "." + year);
 	document.getElementById('todaysdate').innerHTML = activeDate;*/
   	
+function processFormData() {
 	
-function calculateInsurancePerCountry(horse_power,age,country) {
+	var formFirstLastName = document.getElementById("firstlastname");
+	var name = formFirstLastName.value;
+	var formAge = document.getElementById("age");
+	var age = formAge.value;
+	var formBhp = document.getElementById("bhp");
+	var horsePower = formBhp.value;
+	
+	var selected_index = myForm.elements["country"].selectedIndex;
+
+	console.log(selected_index);
+	
+	if(selected_index = [1]) {
+			var insurance = Math.round(Number(horsePower)*100/Number(age)+50);
+
+	}
+	else if (selected_index = [2]) {
+			var insurance = Math.round(Number(horsePower)*120/Number(age)+100);
+	}
+	else if (selected_index = [3]) {
+			var insurance = Math.round(Number(horsePower)*150/(Number(age)+3)+50);
+	}
+	
+	document.getElementById("resultoutput").innerHTML = "<h2> Hi " + name + " your auto insurance per year is €" + insurance + ".</h2>";
+	console.log("Hi " + name + "your auto insurance is per year €" + insurance + "!");
+}
+
+/*function calculateInsurancePerCountry(horse_power,age,country) {
  	var austria = Math.round(horse_power*100/age+50);
  	var hungary = Math.round(horse_power*120/age+100);
  	var greece = Math.round(horse_power*150/(age+3)+50);
  	var country = [austria, hungary, greece];
  	return country
- } 
 
-	var insurance = calculateInsurancePerCountry(150,41);
+ 	var insurance = calculateInsurancePerCountry(horsePower,age);
 	console.log(insurance);
+ } */
+	
 
 
-function getForm() {
-	var formFirstLastName = document.getElementById("firstlastname").value;
-	var formAge = document.getElementById("age").value;
-	var formCountry = document.getElementById("country").value;
-	var formBhp = document.getElementById("bhp").value;
-	var formData = [formFirstLastName, formAge, formCountry, formBhp]
-	return formData
-}	
-
-	var allFormData = getForm();
+	/*var allFormData = processFormData();
 	console.log(allFormData);
 	
  
  	var m = document.getElementById("calcBtn");
-	m.addEventListener('submit',getForm(), false);
+	m.addEventListener('submit',processFormData(), false);
 
  	document.getElementById("resultoutput").innerHTML = "Congratulations your insurance will be: <br> €" + insurance[0] + " per year!";
-
+*/
 
 
 
